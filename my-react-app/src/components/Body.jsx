e735288492005e4be04611675264d85a0a384e99
Mahmud Abdulazeez <abdulazeezm578@gmail.com>
2022-10-28 16:11:29 +0100

    Added 3 components: header, body and footer. And also their contents

--- my-react-app/src/components/Body.jsx
+++ my-react-app/src/components/Body.jsx
@@ -1,7 +1,30 @@
+import slackImage from "../assets/slack.png"
+import githubImage from "../assets/github-icon.png"
+
 export default function Body() {
+    const myTwitterLink = "https://twitter.com/MahmudEnesi?t=64VdroylYD7_shpdaB5fhQ&s=09";
+    const zuriTraining = "https://training.zuri.team/"
+    const books = "http://books.zuri.team/";
+    const book_python = "https://books.zuri.team/python-for-beginners?ref_id=Enesi";
+    const pitch = "https://background.zuri.team";
+    const design_rules = "https://books.zuri.team/design-rules";
+
     return (
-        <div className="body">
-            
+        <div className="wrapper">
+            <div className="body">
+                <a href={myTwitterLink} id="myTwitterLink">Twitter Link</a>
+                <a href={zuriTraining} id="btn__zuri">Zuri Team</a>
+                <a href={books} id="books">Zuri Books</a>
+                <a href={book_python} id="book__python">Python Books</a>
+                <a href={pitch} id="pitch">Background Check for Coders</a>
+                <a href={design_rules} id="book__design">Design Books</a>
+            </div>
+            <div className="socials">
+                <img src={slackImage} alt="slack image" />
+                <a href="https://github.com/Mahmud1087"><img src={githubImage} alt="github image" /></a>
+            </div>
+
+
         </div>
     )
 }
--- my-react-app/src/components/Header.jsx
+++ my-react-app/src/components/Header.jsx
@@ -1,9 +1,15 @@
 import profile_img from "../assets/profile__img.png"
+import share_mobile from "../assets/mobile_Avatar share button.png"
+import share_large from "../assets/_Avatar share button.png"
 
 export default function Header() {
     return (
         <div className="header">
-            <img src={profile_img} alt="" id="profile_img" />
+            <img src={profile_img} alt="profile picture" id="profile_img" className="profile_img" />
+            <h4 id="twitter">Abdulazeez Mahmud</h4>
+            <h4 className="slack" id="slack">Enesi</h4>
+            <img src={share_mobile} alt="Mobile share button" className="share_mobile" />
+            <img src={share_large} alt="Medium and large screens share button" className="share_large" />
         </div>
     )
 }
