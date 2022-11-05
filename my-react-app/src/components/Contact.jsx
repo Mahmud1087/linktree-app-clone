7672b139da9ecfb67ff36d23339fa9ce2f19f418
Mahmud Abdulazeez <abdulazeezm578@gmail.com>
2022-11-05 07:20:59 +0100

    feat: Added the 'Contact' component which has form in it to the app

--- my-react-app/src/components/Contact.jsx
+++ my-react-app/src/components/Contact.jsx
@@ -1,5 +1,66 @@
+import { Link } from "react-router-dom"
+
 export default function Contact() {
     return (
-        <h1>Hello From Contact Page</h1>
+        <div className="contact">
+            <div className="contact--header">
+                <h2>Contact Me</h2>
+                <p>Hi there, contact me to ask me about anything you have in mind</p>
+            </div>
+
+            <form>
+                <div className="form--firstname_lastname">
+                    <label className="form--label">First Name
+                        <input 
+                            type="text"
+                            id="first_name"
+                            placeholder="Enter your first name"
+                            className="form--name"
+                        />
+                    </label>
+
+                    <label className="form--label">Last Name
+                        <input 
+                            type="text"
+                            id="last_name"
+                            placeholder="Enter your first name"
+                            className="form--name"
+                        />
+                    </label>
+                </div>
+                
+                <label className="form--label">Email
+                    <input 
+                        type="email"
+                        id="email"
+                        placeholder="yourname@email.com"
+                        className="form--email"
+                    />
+                </label>
+
+                <label className="form--label">Message
+                    <textarea  
+                        id="message" 
+                        cols="30" 
+                        rows="7"
+                        placeholder="Send me a message and I'll reply you as soon as possible...">
+                    </textarea>
+                </label>
+
+                <div className="form--check_sendBtn">
+                    <label className="checkBox">You agree to providing you data to {"{name}"} who may contact you
+                        <input 
+                            type="checkbox"
+                            className="check" 
+                        />
+                        <span className="box"></span>
+                    </label>
+                    <Link 
+                        to="/success" 
+                        element={<h2>Success!</h2>}>Send message
+                    </Link>
+                </div>
+            </form>
+        </div>
     )
 }
